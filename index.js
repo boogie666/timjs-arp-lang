a55bb69fe69709cedbe54bfa981b0a97d8a55dd6
boogie666 <bogdan.bugarschi@visualretailing.com>
2018-02-12 14:54:24 +0200

    removed comments for now

--- index.js
+++ index.js
@@ -1,64 +1,21 @@
 
 
-// a basic 'scope' for our language...
-// it will never have globals...
-// so it will thorw by default.
 function ctx(name){
     throw name + " is undefined";
 }
 
-/**
-   Eval function.
-
-   Our language will only have 4 types of things.
-
-   values (numbers or booleans),
-   symbols (represented by strings),
-   special forms (what would normaly be 'keywords' in other languages)
-   and functions.
-
-   the eval_expr function returns the result of the evaluated expression.
-
-   params:
-    expr - the expresion to evaluate.
-    ctx - the current scope from which it will get refs.
- */
 function eval_expr(expr, ctx){
-    //the numbers.
     if(typeof expr === "number"){
         return expr;
     }
-    //symbols (or variables), represented by strings
     if(typeof expr === "string"){
         return ctx(expr);
     }
 
-
-    //at this point the valid things left in our language
-    // are arrays. so we can just assume that's the case
-
-    //first we need some conditionals in our language
-    // we're only going to do the basic 'if' expression.
-    // this would be somewhat analogues to javascripts ternary operator ( the a ? b : c thing )
-
-    //our if will look like this:
-    // ["if", "condition_expr",
-    //    "then_expr",
-    //    "else_expr"]
-
-    // 'if' will only ever evaluate either the 'then_expr' or 'else_expr' based on the 'condition_expr'
-    // we're just gonna use basic js truthiness here.
-    // so that evals to true in javascript will be true in our lang.
     if(expr[0] === "if"){
         const test = expr[1];
         const t = expr[2];
         const f = expr[3];
-        // this looks like cheeting...
-        // because we use the host 'if' to implement our 'if'
-        // but, think about it....
-        // if we were to compile to machine code.
-        // we'd still use the 'host' if... a conditional jump in the case of machine code.
-        // so it's not really cheeting. it's more 'being clever' :P
         if(eval_expr(test, ctx)){
             return eval_expr(t, ctx);
         }else {
@@ -66,61 +23,30 @@ function eval_expr(expr, ctx){
         }
     }
 
-    //next we define what a function definition looks like in our lang.
-    // a function will be defined in json syntax like so
-    // ["function", ["arg1", "arg2"] [... body ...]]
-
-    // so any array that has the 'keyword' "function" in the first position
-    // and another array strings in the second position
-    // and any valid expression in the thrid position
-    // will define a function.
     if(expr[0] === "function" && expr[1] instanceof Array){
         const function_arguments = expr[1];
         const function_body = expr[2];
-        // we represent a function in our language
-        // as a function in the host language (i.e. a javascript function)
         return function(/*args*/){
-            //the function will be passed arguments that we need to add to our local scope
             const arg_values = Array.from(arguments);
-            //we create a new function to act as the local scope for our function.
             const local_scope = function(name_of_argument){
-                // first we need to look in the function_arguments.
-                // if the function body uses it's own arguments
-                // we need to look them up by name
-                // and return the value associated with that argument.
                 for(var i = 0; i < function_arguments.length; i++){
                     if (function_arguments[i] === name_of_argument){
                         return arg_values[i];
                     }
                 }
 
-                //if the value is not in local_scope
-                //we defer to the scope above (that will be either the global scope or some encloseing function.)
                 return ctx(name_of_argument);
             };
 
-            //lastly we need to eval the function_body with it's local_scope.
             return eval_expr(function_body, local_scope);
         };
     }
 
-    //lastly we need a way of actually calling function defined in our own language
-    // here we'll do the lisp thing...
-    // basically arrays === calling a function.
-    // ["inc" 1] is equivalnet to javascript's inc(1);
-
-    // so if we ever see an array, we just assume that it's a function call at this point.
-
-    // !!note that function definition is also an array!!
-    // but because we've already handled that case above.
-    // we know that we our interperter ever get's here, we have a function call.
     if(expr instanceof Array){
-        const the_function = expr[0]; // the function is the first thing in the array
-        const evaluated_function = eval_expr(the_function, ctx); // we need to eval 'the_function', because it can be either a symbol or a annonymus function that we need to create into an actuall function.
-        const the_arguments = expr.slice(1); //the arguments are everything else
-        const evaluated_arguments = the_arguments.map(x => eval_expr(x, ctx)); // we need to eval the arguments before passing them to our function.
-        // remember, our functions, are represented by regular js functions.
-        // so we can just use the .apply method on them, passing in the evaluated arguments.
+        const the_function = expr[0]; 
+        const evaluated_function = eval_expr(the_function, ctx); 
+        const the_arguments = expr.slice(1); 
+        const evaluated_arguments = the_arguments.map(x => eval_expr(x, ctx)); 
         return evaluated_function.apply(null, evaluated_arguments);
     }
     throw JSON.stringify(expr) + " is not a valid expression.";
